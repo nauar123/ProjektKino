34e3c1a647a5c6640aa8c227f4bd62c2658f729c
nauar123 <nauarbahaa@gmail.com>
2025-09-26 15:07:36 +0200

    Projekt oprettet
    Projekt oprettet

---
+++ src/main/java/com/example/projektkino/ProjektKinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektkino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektKinoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektkino/ProjektKinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektkino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
